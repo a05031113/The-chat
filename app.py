4bd22d9ab2772b05625d7545da49a9d2bb788320
a05031113 <a05031113@gmail.com>
2022-12-27 23:00:06 +0800

    test

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask
+from application import *
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, Docker!'
-
+app = create_app()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000, debug=True)
